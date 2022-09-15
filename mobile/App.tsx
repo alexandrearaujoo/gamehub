4ee4a3a60e404cd50f3194e7b7a7fdab569649eb
Alexandre Araujo <alexandre01393@gmail.com>
2022-09-15 20:51:48 -0300

    feat: notifications done

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -10,6 +10,12 @@ import { Background } from "./src/components/Background";
 import { Loading } from "./src/components/Loading";
 import { Routes } from "./src/routes";
 
+import "./src/services/notification.config";
+import { useEffect, useRef } from "react";
+import { Subscription } from "expo-modules-core";
+import { getPushNotificationToken } from "./src/services/getPushNotification";
+import * as Notifications from "expo-notifications";
+
 export default function App() {
   const [fontsLoading] = useFonts({
     Inter_400Regular,
@@ -17,6 +23,35 @@ export default function App() {
     Inter_700Bold,
     Inter_900Black,
   });
+
+  const getNotificationListener = useRef<Subscription>();
+  const responseNotificationListener = useRef<Subscription>();
+
+  useEffect(() => {
+    getPushNotificationToken();
+  }, []);
+
+  useEffect(() => {
+    getNotificationListener.current =
+      Notifications.addNotificationReceivedListener((notification) => {});
+
+    responseNotificationListener.current =
+      Notifications.addNotificationResponseReceivedListener((res) => {});
+
+    return () => {
+      if (
+        getNotificationListener.current &&
+        responseNotificationListener.current
+      ) {
+        Notifications.removeNotificationSubscription(
+          getNotificationListener.current
+        );
+        Notifications.removeNotificationSubscription(
+          responseNotificationListener.current
+        );
+      }
+    };
+  }, []);
 
   return (
     <Background>
